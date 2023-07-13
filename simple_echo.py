0a066dae2f6cab1f055526fcd839675d6c90b0c2
Yuvraj Singh <yuvisingh478@gmail.com>
2023-07-13 15:27:29 +0530

    tinkered with WS; tracking opened and closed connections

--- simple_echo.py
+++ simple_echo.py
@@ -1,16 +1,40 @@
 import asyncio
-from websockets.server import serve
+from websockets.server import serve, WebSocketServerProtocol
 
 
-async def echo(websocket):
-    async for message in websocket:
-        print(f"Sending message: {message}")
-        await websocket.send(message)
+async def hello(websocket: WebSocketServerProtocol) -> None:
+    # anything that returns `Coroutine` needs to be awaited
+
+    name = await websocket.recv()  # await for the message to be recieved
+    print(f"<<< {name}")
+
+    greetings = f"hello {name}"
+    await websocket.send(greetings)  # send the message as a greeting
+    print(f">>> {greetings}")
 
 
-async def main():
-    async with serve(echo, "localhost", 8765):
+async def hello_many(wsserver: WebSocketServerProtocol) -> None:
+    # for every incomming data stream from the same ws connection
+    # await for it, then just echo the data back
+    client_id = wsserver.id
+    print(f"connection made with: {client_id}")
+
+    async for message in wsserver:
+        print(f"<< {message}")
+
+        await wsserver.send(message)
+        print(f">> {message}")
+
+    print(f"connection closed with: {client_id}.")
+    print("waiting for other connection")
+
+
+async def main() -> None:
+    # create a connection handler to serve
+
+    async with serve(hello_many, "localhost", 8765):
         await asyncio.Future()  # run forever
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
